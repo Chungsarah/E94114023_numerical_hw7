80fdd9fe6a1751dabaf0ed257379a0f5d07ba7db
Chungsarah <113426722+Chungsarah@users.noreply.github.com>
2025-05-09 00:07:14 +0800

    Add files via upload

--- E94114023_numerical_hw7.py
+++ E94114023_numerical_hw7.py
@@ -1,52 +1,110 @@
 import numpy as np
-A=np.array(
+A=np.array([
     [4,-1,0,-1,0,0],
     [-1,4,-1,0,-1,0],
     [0,-1,4,0,1,-1],
     [-1,0,0,4,-1,-1],
     [0,-1,0,-1,4,-1],
     [0,0,-1,0,-1,4]
-)
-U=np.array(
+])
+U=np.array([
     [0,-1,0,-1,0,0],
     [0,0,-1,0,-1,0],
     [0,0,0,0,1,-1],
     [0,0,0,0,-1,-1],
     [0,0,0,0,0,-1],
     [0,0,0,0,0,0]
-)
-L=np.array(
+])
+L=np.array([
     [0,0,0,0,0,0],
     [-1,0,0,0,0,0],
     [0,-1,0,0,0,0],
     [-1,0,0,0,0,0],
     [0,-1,0,-1,0,0],
     [0,0,-1,0,-1,0]
-)
-D=np.array(
+])
+D=np.array([
     [4,0,0,0,0,0],
     [0,4,0,0,0,0],
     [0,0,4,0,0,0],
     [0,0,0,4,0,0],
     [0,0,0,0,4,0],
     [0,0,0,0,0,4]
-)
-bt=np.array([0,-1,9,4,8,6])
+])
+bt=np.array([[0,-1,9,4,8,6]])
 B=bt.T
 def jacobi(a,u,l,d,b):
-    xt=np.zeros(6)
+    xt=np.array([[1,1,1,1,1,1]])
     X=xt.T
-    Dv=d.linalg.inv()
-    T=-1*Dv.dot(u+l)
-    C=Dv.dt(b)
-    e=a.dot(X)-b
-    err=e.linalg.det
-    while abs(err)>0.001:
-        Xnew=T.dot(X)+C
+    Dv=np.linalg.inv(d)
+    T=-1*(Dv@(u+l))
+    C=Dv@b
+    print('X0:')
+    print(X)
+    for i in range(1,50):
+        Xnew=T@X+C
+        print('X'+str(i)+':')
+        print(Xnew)
+        if np.linalg.norm(Xnew-X)<0.001:
+           return Xnew
         X=Xnew
-        e=a.dot(X)-b
-        err=e.linalg.det
-    return X
+def gauss_seidel(a,u,l,d,b):
+    xt=np.array([[1,1,1,1,1,1]])
+    X=xt.T
+    T=-1*(np.linalg.inv(d+l)@u)
+    C=np.linalg.inv(d+l)@b
+    print('X0:')
+    print(X)
+    for i in range(1,50):
+        Xnew=T@X+C
+        print('X'+str(i)+':')
+        print(Xnew)
+        if np.linalg.norm(Xnew-X)<0.001:
+           return Xnew
+        X=Xnew
+def SOC(a,u,l,d,b,w):
+    xt=np.array([[1,1,1,1,1,1]])
+    X=xt.T
+    T=np.linalg.inv(d+w*l)@((1-w)*d-w*u)
+    C=w*np.linalg.inv(d+l)@b
+    print('X0:')
+    print(X)
+    for i in range(1,50):
+        Xnew=T@X+C
+        print('X'+str(i)+':')
+        print(Xnew)
+        if np.linalg.norm(Xnew-X)<0.001:
+           return Xnew
+        X=Xnew
+def TCG(a,b):
+    xt=np.array([[1,1,1,1,1,1]])
+    X=xt.T
+    print('X0:')
+    print(X)
+    for i in range(1,50):
+        V=b-a@X
+        t=(V.T@V)/(V.T@a@V)
+        Xnew=X+t*V
+        print('X'+str(i)+':')
+        print(Xnew)
+        if np.linalg.norm(Xnew-X)<0.001:
+           return Xnew
+        X=Xnew
+print('A=')
+print(A)
+print('B=')
+print(B)
+'''
+print('(a)Jacobi method:')
+jacobi(A, U, L, D, B)
+print('(b)Gauss_seidel method:')
+gauss_seidel(A, U, L, D, B)
+'''
+print('(c)SOC method:')
+SOC(A, U, L, D, B,0.5)
+print('(d)The conjugate gradient method:')
+TCG(A, B)
+
 
 
 
